08b34ee7e468b9ee29bdabd0aa9aaab35567d7f5
seveniruby <seveniruby@gmail.com>
2019-08-29 22:08:49 +0800

    capabilties与更多action的进阶

--- test_appium/test_xueqiu.py
+++ test_appium/test_xueqiu.py
@@ -7,7 +7,9 @@ from appium import webdriver
 import pytest
 from appium.webdriver.common.mobileby import MobileBy
 from hamcrest import *
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -19,7 +21,20 @@ class TestXueqiu:
         caps["deviceName"] = "hogwarts"
         caps["appPackage"] = "com.xueqiu.android"
         caps["appActivity"] = ".view.WelcomeActivityAlias"
-        caps['autoGrantPermissions'] = True
+        #caps['autoGrantPermissions'] = True
+        #caps["avd"]="Pixel_2_API_27"
+        #caps["networkSpeed"]="gsm"
+        #caps["dontStopAppOnReset"]="true"
+        caps["noReset"]="true"
+
+        #fast
+        caps["skipUnlock"]=True
+        caps["skipLogcatCapture"]=True
+        caps["disableAndroidWatchers"]=True
+        caps["ignoreUnimportantViews"]=True
+        caps["skipServerInstallation"]=True
+        caps["systemPort"]=6790
+
         #todo： 使用path变量更可靠
         #caps["chromedriverExecutableDir"] = '/Users/seveniruby/projects/chromedriver/2.20/'
         #caps["chromedriverUseSystemExecutable"]=True
@@ -40,7 +55,8 @@ class TestXueqiu:
 
             return size >= 1
 
-        WebDriverWait(self.driver, 10, 1).until_not(click_cancel)
+        #WebDriverWait(self.driver, 10, 1, ignored_exceptions=[TimeoutException]).until(expected_conditions.visibility_of_element_located((By.ID, 'image_cancel')))
+        #WebDriverWait(self.driver, 5, 1, ignored_exceptions=[TimeoutException]).until_not(click_cancel)
 
         self.driver.find_element_by_id("user_profile_icon")
 
@@ -118,6 +134,37 @@ class TestXueqiu:
         self.driver.find_element(By.NAME, "tel").send_keys("15600534760")
         self.driver.find_element(By.NAME, "captcha").send_keys("1234")
         self.driver.find_element(By.CSS_SELECTOR, ".dj-button").click()
+
+    def test_screenshot(self):
+        print(self.driver.start_recording_screen())
+        self.driver.save_screenshot("1.png")
+        trade=self.driver.find_element(MobileBy.XPATH, "//*[@text='交易']")
+        trade.screenshot("2.png")
+        trade.click()
+        #self.driver.orientation
+        sleep(3)
+        self.driver.stop_recording_screen()
+
+
+    def test_log(self):
+        print(self.driver.log_types)
+        print(self.driver.get_log("logcat"))
+
+
+    def test_network(self):
+        self.driver.send_sms("15600534760", "hello veryone, from hogwarts")
+        sleep(2)
+        self.driver.make_gsm_call("15600534760", "call")
+
+    def test_perf(self):
+        print(self.driver.get_performance_data_types())
+        sleep(20)
+
+        for p in self.driver.get_performance_data_types():
+            print(self.driver.get_performance_data("com.xueqiu.android", p))
+
+
+
 
 
 
